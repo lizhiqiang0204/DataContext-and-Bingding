9e6ce00ab1bcdd001a6fc8395877c93b16936c52
lizhiqiang0204 <18518092079@163.com>
2020-02-29 13:06:28 +0800

    first commit

---
+++ WpfApp1/WpfApp1/ClassA.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WpfApp1
+{
+    public class ClassA
+    {
+        private string label = "新建类A的标签";
+        public string myLabel
+        {
+            get { return label; }
+            set { label = value; }
+        }
+    }
+}
---
+++ WpfApp1/WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System.Windows;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private string label = "我的标签";
+        public string myLabel
+        {
+            get { return label; }
+            set { label = value; }
+        }
+        public MainWindow()
+        {
+            InitializeComponent();
+            //DataContext = this;
+            DataContext = new ClassA();
+        }
+    }
+}
